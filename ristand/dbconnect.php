9ba66aeca33d1e812116f8b668939c76281e13d8
Elia Sabaru <89246134+Science06@users.noreply.github.com>
2022-06-15 02:49:47 +0700

    Add files via upload

---
+++ ristand/dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","","Ristand");
+
+if(!$conn){
+	echo "gagal konek database";
+} else {
+	
+};
+
+?>
